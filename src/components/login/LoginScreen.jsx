668517d3e5c16e2d5b6f2601ad0aed8d66d7deb9
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-08-31 19:52:58 -0500

    seteando el replace

--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+/**
+ *
+ * @param {History Annotation} param0 History es nativo de los props y se usa para ver el historial del componente
+ */
+const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    history.replace("/");
+  };
 
-const LoginScreen = (props) => {
   return (
-    <>
-      <div>LoginScreen</div>
-    </>
+    <div className="container mt-5">
+      <h1>LoginScreen</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Ingresar
+      </button>
+    </div>
   );
 };
 

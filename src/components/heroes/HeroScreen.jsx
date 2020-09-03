81742afb7c732c9329f7a395e05a9c9cbbb95db4
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-02 21:40:22 -0500

    useparams

--- src/components/heroes/HeroScreen.jsx
+++ src/components/heroes/HeroScreen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams, Redirect } from "react-router-dom";
 import { getHeroById } from "../../selectors/getHeroById";
 
-const HeroScreen = (props) => {
+const HeroScreen = ({ history }) => {
   // Extrae parametros del url
   const { heroeId } = useParams();
 
@@ -20,10 +20,43 @@ const HeroScreen = (props) => {
     characters,
   } = hero;
 
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push("/");
+    } else {
+      history.goBack();
+    }
+  };
+
   return (
-    <>
-      <div>HeroesScreen</div>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/${heroeId}.jpg`}
+          className="img-thumbnail"
+          alt={superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Nombre: {alter_ego}</b>
+          </li>
+          <li className="list-group-item">
+            <b>Publicado por: {publisher}</b>
+          </li>
+          <li className="list-group-item">
+            <b>Primera aparción: {first_appearance}</b>
+          </li>
+        </ul>
+        <h5>Personajes</h5>
+        <p>{characters}</p>
+        <button onClick={handleReturn} className="btn btn-outline-info">
+          Regresar
+        </button>
+      </div>
+    </div>
   );
 };
 
